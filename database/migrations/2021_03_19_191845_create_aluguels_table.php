f9101c98073b3ccd27127eafd82cd192e689bcb3
unknown <ayranlouro@hotmail.com>
2021-03-20 15:39:44 -0300

    Begin

---
+++ database/migrations/2021_03_19_191845_create_aluguels_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAluguelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aluguels', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('carro_id');
+            $table->dateTime('data_inicio_periodo');
+            $table->dateTime('data_final_previsto_periodo');
+            $table->dateTime('data_final_realizado_periodo');
+            $table->float('valor_diaria', 30);
+            $table->integer('km_inicial');
+            $table->integer('km_final');
+            $table->timestamps();
+
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->foreign('carro_id')->references('id')->on('carros');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aluguels');
+    }
+}
